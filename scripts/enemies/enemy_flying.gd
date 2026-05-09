d266b9679469ec80f25f8c5f77a30d03fc6324e2
jake6724 <jakealdridge6724@gmail.com>
2026-05-09 13:27:06 -0400

    Update enemy_flying ai

--- scripts/enemies/enemy_flying.gd
+++ scripts/enemies/enemy_flying.gd
@@ -1,98 +1,45 @@
 class_name EnemyFlying extends Enemy
 
 @export var player: Player # TODO: Eventually this should be detected in an area3d
+@export_group("Stats")
+@export var acceleration = 30
+@export var speed = 8
+@export_group("Components")
+@export var raycast_detect_top: RayCast3D
+@export var raycast_detect_center: RayCast3D
+@export var raycast_detect_bottom: RayCast3D
+@export_group("Debug")
+@export var show_debug: bool = false
 @export var move_indicator: MeshInstance3D
-@export var raycast_player_detect_top: RayCast3D
-@export var raycast_player_detect_center: RayCast3D
-@export var raycast_player_detect_bottom: RayCast3D
 @export var last_seen_player_position_indicator: MeshInstance3D
 
-var acceleration = 30
-var speed = 8
-
-var can_spot_player: bool = true ## TODO: Is this even used?
-@export var timer_spot_player: Timer
-var can_spot_player_cooldown: float = 0.5
-
+# Direction
 var directions: Array[Vector3] = [] # Generated based on num_directions
 var num_directions: float = 128.0
-
+# Steering
 var interest: Array[float]
 var danger: Array[float]
-
+# Danger Raycasts
 var danger_raycasts: Array[RayCast3D]
-
-var danger_distance: float = 4
-
+var danger_raycast_length: float = 4
+# LSP
 var last_seen_player_position: Vector3
 var last_seen_player_position_reached: bool = false
 var last_seen_player_position_reached_threshold: float = .5
-
+# Scents
 var ignore_scents: Dictionary[Scent, Variant] = {}
-var scent_reached_threshold
-
+var scent_reached_threshold: float = 2.0 # Could try diff values still
 
 func _ready():
 	super()
 	directions = create_directions(num_directions)
 	create_danger_raycasts()
 
-	timer_spot_player.timeout.connect(on_timer_spot_player)
-
-func on_timer_spot_player() -> void:
-	can_spot_player = true
+	move_indicator.visible = show_debug
+	last_seen_player_position_indicator.visible = show_debug
 
 func _physics_process(delta):
-	var move_target_point: Vector3
-	
-	# TODO: The biggest issue right now is when the enemy wants to reach last seen position, but it is stuck on a wall
-	# Atleast with the scents they despawn and its goal changes which can help fix itself. can't do this with LSP
-	# Maybe a timer? Or higher avoidance ?
-
-	# Can see player
-	if is_target_visible(player.tracker.global_transform.origin):
-		move_target_point = player.tracker.global_transform.origin
-		last_seen_player_position = player.tracker.global_transform.origin
-		last_seen_player_position_reached = false
-		last_seen_player_position_indicator.get_surface_override_material(0).albedo_color = Color.GREEN
-		ignore_scents.clear()
-		
-		for scent in player.scents.slice(0,player.scents.size()-2,1): # Discard any scents spawned at the moment or before we saw the player, except the last one (always want atleast 1 available to follow)
-			ignore_scents.set(scent, null)
-
-	# Cannot see player, hasn't visited LSP. Move to LSP
-	elif not last_seen_player_position_reached and is_target_visible(last_seen_player_position):
-		move_target_point = last_seen_player_position
-		can_spot_player = false
-		timer_spot_player.start(can_spot_player_cooldown)
-
-	# Cannot see player, HAS visited LSP, OR cannot see LSP. Follow scent trail
-	elif player.scents.size() != 0:
-		# Set debug values
-		last_seen_player_position_reached = true
-		last_seen_player_position_indicator.get_surface_override_material(0).albedo_color = Color.PALE_GREEN
-
-		# Find closest scent and follow
-		var closest_scent: Scent = get_closest_scent(player)
-		if closest_scent:
-			move_target_point = closest_scent.global_transform.origin
-		# else:
-		# 	push_warning("No scents available for EnemyFlying to follow")
-		# 	velocity = Vector3.ZERO
-		# 	return
-	
-	# Can't see player, no scent trail. Give up and stand still
-	else:
-		velocity = Vector3.ZERO
-		return
-
-	# Update debug indicators
-	# TODO: Add show_debug flag
-	last_seen_player_position_indicator.global_transform.origin = last_seen_player_position
-	if global_transform.origin.distance_to(last_seen_player_position) < last_seen_player_position_reached_threshold:
-		last_seen_player_position_reached = true
-		last_seen_player_position_indicator.get_surface_override_material(0).albedo_color = Color.DARK_GREEN
-
+	var move_target_point: Vector3 = get_move_target_point()
 	interest = get_interest_weights(move_target_point)
 	danger = get_danger_weights()
 	
@@ -113,25 +60,73 @@ func _physics_process(delta):
 	var move_direction: Vector3 = get_move_direction()
 	velocity = velocity.move_toward(move_direction * speed, delta * acceleration)
 
-	move_indicator.global_transform.origin = global_transform.origin + (velocity.normalized() * 2)
-
 	move_and_slide()
+
+	# Update debug indicators
+	move_indicator.global_transform.origin = global_transform.origin + (velocity.normalized() * 2)
+	last_seen_player_position_indicator.global_transform.origin = last_seen_player_position
+	if global_transform.origin.distance_to(last_seen_player_position) < last_seen_player_position_reached_threshold:
+		last_seen_player_position_reached = true
+		last_seen_player_position_indicator.get_surface_override_material(0).albedo_color = Color.DARK_GREEN
+
+	print(ignore_scents.size())
+
+func get_move_target_point() -> Vector3:
+	var _move_target_point: Vector3
+	# Can see player
+	if is_target_visible(player.tracker.global_transform.origin):
+		# Update last seen position for use later
+		_move_target_point = player.tracker.global_transform.origin
+		last_seen_player_position = player.tracker.global_transform.origin
+		last_seen_player_position_reached = false
+		last_seen_player_position_indicator.get_surface_override_material(0).albedo_color = Color.GREEN
+		
+		# Discard any scents spawned before we saw the player, 
+		# except the last one (always want atleast 1 available to follow)
+		for scent in player.scents.slice(1,player.scents.size(),1):
+			ignore_scents.set(scent, null)
+			scent.mesh.get_surface_override_material(0).albedo_color = Color.PINK
+
+		# Issues erasing old scents (https://github.com/godotengine/godot/issues/110511)
+		# for scent in ignore_scents.keys():
+		# 	if not is_instance_valid(scent):
+		# 		ignore_scents.erase(scent)
+
+	# Cannot see player, hasn't visited LSP. Move to LSP
+	elif not last_seen_player_position_reached and is_target_visible(last_seen_player_position):
+		_move_target_point = last_seen_player_position
+
+	# Cannot see player, HAS visited LSP, OR cannot see LSP. Follow scent trail
+	elif player.scents.size() != 0:
+		# Set debug values
+		last_seen_player_position_reached = true
+		last_seen_player_position_indicator.get_surface_override_material(0).albedo_color = Color.PALE_GREEN
+
+		# Find closest scent position and follow
+		_move_target_point = get_closest_scent_position(player)
+	
+	# Can't see player, no scent trail. Return empty Vector3
+	else:
+		_move_target_point = Vector3()
+
+	return _move_target_point
 
 ## Checks if each raycast (top, center, bottom) can see the target position. The raycasts are not looking for the target, they only collide with
 ## terrain and have infinite range. If no collisions are found, then it is possible to see that location.
 ## Returns positive if target IS visible, false otherwise.
 ## Updates the `target_position` of each detection raycast and calls `force_raycast_update()` on each BEFORE checking for collisions
 func is_target_visible(_move_target_point) -> bool:
-	set_raycasts_target(_move_target_point)
-	return not raycast_player_detect_top.is_colliding() and not raycast_player_detect_bottom.is_colliding() and not raycast_player_detect_center.is_colliding()
+	set_detection_raycasts_target(_move_target_point)
+	return not raycast_detect_top.is_colliding() and not raycast_detect_bottom.is_colliding() and not raycast_detect_center.is_colliding()
 
-func set_raycasts_target(_move_target_point: Vector3) -> void:
-	raycast_player_detect_top.target_position = to_local(_move_target_point)
-	raycast_player_detect_bottom.target_position = to_local(_move_target_point)
-	raycast_player_detect_center.target_position = to_local(_move_target_point)
-	raycast_player_detect_top.force_raycast_update()
-	raycast_player_detect_bottom.force_raycast_update()
-	raycast_player_detect_center.force_raycast_update()
+## Set the `target_position` of each detection raycast and call `force_raycast_update()` on each
+func set_detection_raycasts_target(_move_target_point: Vector3) -> void:
+	raycast_detect_top.target_position = to_local(_move_target_point)
+	raycast_detect_bottom.target_position = to_local(_move_target_point)
+	raycast_detect_center.target_position = to_local(_move_target_point)
+	raycast_detect_top.force_raycast_update()
+	raycast_detect_bottom.force_raycast_update()
+	raycast_detect_center.force_raycast_update()
 
 ## Calculate the weight for each direction based on the dot product of that direction and player position.
 ## The move direct a direction points to the player, the higher the weight
@@ -153,7 +148,7 @@ func get_danger_weights() -> Array[float]:
 		if danger_raycasts[i].is_colliding():
 			var collision_point: Vector3 = danger_raycasts[i].get_collision_point()
 			var distance_to_collision: float = global_transform.origin.distance_to(collision_point)
-			var distance_scale: float = distance_to_collision / danger_distance
+			var distance_scale: float = distance_to_collision / danger_raycast_length
 			var weight: float = 1 - (1 * distance_scale)
 			_danger[i] = weight
 		else:
@@ -170,7 +165,7 @@ func create_danger_raycasts() -> void:
 		new_raycast.global_transform.origin = global_transform.origin
 
 		# Point and scale out raycast, configure
-		new_raycast.target_position = directions[i] * danger_distance
+		new_raycast.target_position = directions[i] * danger_raycast_length
 		new_raycast.debug_shape_custom_color = Color.ORANGE_RED
 		new_raycast.collision_mask = 4
 		danger_raycasts.append(new_raycast)
@@ -192,16 +187,25 @@ func create_directions(_num_directions: float) -> Array[Vector3]:
 		res.append(_direction)
 	return res
 
-func get_closest_scent(_player: Player) -> Scent:
-	var closest_scent: Scent
+## Return the closest player scent. Check if Player.scents is populated BEFORE using this function, or it may return an empy Scent object
+func get_closest_scent_position(_player: Player) -> Vector3:
+	var closest_scent_position: Vector3
+	var closest_scent: Scent # Only used for debug
 	var closest: float = INF
 	for scent in player.scents:
-		var scent_distance: float = global_transform.origin.distance_to(scent.global_transform.origin)
-		if scent not in ignore_scents:
-			if scent_distance < closest:
-				closest_scent = scent
-				closest = scent_distance
+		if is_instance_valid(scent):
+			var scent_distance: float = global_transform.origin.distance_to(scent.global_transform.origin)
+			if scent not in ignore_scents:
+				scent.mesh.get_surface_override_material(0).albedo_color = Color.YELLOW
+				if (scent_distance < closest) and is_target_visible(scent.global_transform.origin):
+					closest_scent_position = scent.global_transform.origin
+					closest_scent = scent
+					closest = scent_distance
 
-			if scent_distance <= scent_reached_threshold: # Ignore this scent in the future if it close enough to it
-				ignore_scents.set(scent, null)
-	return closest_scent
+				if scent_distance <= scent_reached_threshold: # Ignore this scent in the future if it close enough to it
+					ignore_scents.set(scent, null)
+					scent.mesh.get_surface_override_material(0).albedo_color = Color.PINK
+			else:
+				scent.mesh.get_surface_override_material(0).albedo_color = Color.PINK
+	if is_instance_valid(closest_scent): closest_scent.mesh.get_surface_override_material(0).albedo_color = Color.RED
+	return closest_scent_position
--- scripts/player/scent.gd
+++ scripts/player/scent.gd
@@ -1,7 +1,11 @@
 class_name Scent extends Node3D
 
+@export var mesh: MeshInstance3D
 
 signal scent_expired
+
+func _ready():
+    mesh.get_surface_override_material(0).albedo_color = Color.YELLOW
 
 func start_despawn(scent_despawn_delay: float) -> void:
     await get_tree().create_timer(scent_despawn_delay).timeout
